36b63b4a8ca2702fc7af211e5313cb1441e11dd0
phemt91 <piciarelli@gmail.com>
2018-03-22 00:26:10 +0100

    change shell 0.1.0

--- BASICshellruby.rb
+++ BASICshellruby.rb
@@ -1,20 +1,14 @@
 
-#libreria standard di ruby
-#tiene la comand history automaticamente
-#printa a schermo il comand prompt
+
+#!/usr/bin/env ruby
 
 
+$stdout.print"--> "
+#qui il programma va in loop in attesa di input
+$stdin.each_line do |line|
+  pid = fork {
+    exec line
+  }
 
-require 'readline'
-
-while input = Readline.readline("--> ", true)
-  break                       if input == "exit"
-  puts Readline::HISTORY.to_a if input == "crono"
-  puts " {-- ctrl+R for reverse-history search \r\n  -- ctrl+w for delete word --}" if input == "help"
-
-
-  # Remove blank lines from history
-  Readline::HISTORY.pop if input == ""
-
-  system(input)
+  Process.wait pid
 end
