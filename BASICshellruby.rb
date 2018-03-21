7b013d998809362de09256cc5c32b5b2335b45e7
phemt91 <piciarelli@gmail.com>
2018-03-22 00:07:44 +0100

    add help

--- BASICshellruby.rb
+++ BASICshellruby.rb
@@ -1,12 +1,20 @@
 
-#altro test grafico
-prompt = "_ù_ù_ "
+#libreria standard di ruby
+#tiene la comand history automaticamente
+#printa a schermo il comand prompt
 
-print prompt
 
-while (input = gets.chomp)
-  break if input == "chiudi"
+
+require 'readline'
+
+while input = Readline.readline("--> ", true)
+  break                       if input == "exit"
+  puts Readline::HISTORY.to_a if input == "crono"
+  puts " {-- ctrl+R for reverse-history search \r\n  -- ctrl+w for delete word --}" if input == "help"
+
+
+  # Remove blank lines from history
+  Readline::HISTORY.pop if input == ""
 
   system(input)
-  print prompt
 end
