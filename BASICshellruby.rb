3fc49f63deb8f990b98fa992656f59cb940b99a6
phemt91 <piciarelli@gmail.com>
2018-03-22 21:54:11 +0100

    add shellwords:cd and exit

--- BASICshellruby.rb
+++ BASICshellruby.rb
@@ -3,12 +3,26 @@
 #!/usr/bin/env ruby
 
 
-$stdout.print"--> "
-#qui il programma va in loop in attesa di input
-$stdin.each_line do |line|
-  pid = fork {
-    exec line
-  }
+#libreria standard ruby
+require 'shellwords'
+  #creiamo la costante BULTINS
+  #Per il cambio di cartella usiamo il comando di ruby
+  BUILTINS = {   'cd' => lambda {|dir| Dir.chdir(dir) },
+              'exit' => lambda {|code = 0| exit(code.to_i) }
+}
 
-  Process.wait pid
+loop do #loop per il prompt
+  $stdout.print"--> "
+    line = $stdin.gets.chomp #gets per l'input
+                             #chomp per rimuovere i whitespace
+    command, *arguments = Shellwords.shellsplit(line)
+
+    if BUILTINS[command]
+       BUILTINS[command].call(*arguments)
+    else
+        pid = fork {
+          exec line
+        }
+    Process.wait pid
+    end
 end
